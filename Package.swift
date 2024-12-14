d659686409332551c637e777e90b8eba5a6ba7df
xncbf <xncbf12@gmail.com>
2024-12-14 19:21:32 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,11 +13,9 @@ let package = Package(
             targets: ["NMapsGeometry"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "NMapsGeometry",
-            url: "https://repository.map.naver.com/archive/pod/NMapsGeometry/1.0.2/NMapsGeometry.zip",
-            checksum: "3b213dee5ebd5fa2bfbc3feb0258f8fec6d70abd6c71c5fa33324af9c1b8b2e2")
+            path: "./NMapsGeometry.zip"
+        )
     ]
 )
